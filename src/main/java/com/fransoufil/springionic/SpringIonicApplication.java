31b00f61f6953adf372669c389e0bbc7b9d9bf87
fransoufil <francixco.sf@gmail.com>
2021-12-24 13:55:07 -0300

    2.18 - Criando Operacao de Instanciacao

--- src/main/java/com/fransoufil/springionic/SpringIonicApplication.java
+++ src/main/java/com/fransoufil/springionic/SpringIonicApplication.java
@@ -1,13 +1,32 @@
 package com.fransoufil.springionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.fransoufil.springionic.domain.Categoria;
+import com.fransoufil.springionic.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class SpringIonicApplication {
+public class SpringIonicApplication implements CommandLineRunner {
+	
+	@Autowired
+	private CategoriaRepository repo;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringIonicApplication.class, args);
 	}
+	
+	@Override
+	public void run(String... args) throws Exception{
 
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		repo.saveAll(Arrays.asList(cat1, cat2));
+		
+	}
 }
